fdbd0155ee48e390271bd36730687c4c619e36e5
SeoHoJun <ghwns6659@naver.com>
2020-08-26 23:45:07 +0900

    Getter,Setter test

--- src/test/java/thymeleaf_JPA_Mysql/develop_study/LombokTests.java
+++ src/test/java/thymeleaf_JPA_Mysql/develop_study/LombokTests.java
@@ -15,10 +15,24 @@ public class LombokTests {
     }
 
     @Test
-    public void testRequriedArgsConstructor(){
+    public void testRequiredArgsConstructor(){
         Board board = new Board("테스트 제목");
 
         System.out.println(board);
     }
 
+    @Test
+    public void testGetter(){
+        Board board = new Board();
+
+        System.out.println(board.getTitle());
+    }
+
+    @Test
+    public void testSetter(){
+        Board board = new Board();
+        board.setTitle("게시판 제목");
+
+        System.out.println(board.getTitle());
+    }
 }
